02446ecfe50488146dbac7c35b2915215d602361
Iuri Rates <iurirates@189-039-008-046.static.spo.ctbc.com.br>
2020-01-10 08:12:09 -0300

    Commit inicial

---
+++ env/bin/django-admin.py
@@ -0,0 +1,5 @@
+#!/Users/Iuri/Library/Mobile Documents/com~apple~CloudDocs/Desenvolvimento/Particulares/Poc's/python/github/poc-python/env/bin/python
+from django.core import management
+
+if __name__ == "__main__":
+    management.execute_from_command_line()
